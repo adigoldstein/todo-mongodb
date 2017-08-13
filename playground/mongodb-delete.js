f2ddc374cd1d7ffa58a7ab95c5362d8933089e38
Adi <adij82@gmail.com>
2017-08-13 13:05:39 +0300

    add delete methods

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,32 @@
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/todoApp', (err, db) => {
+
+  if (err) {
+    return console.info('Unable to connect MangoDB server. Error:', err);
+  }
+
+  //deleteMany
+  // db.collection('Todos').deleteMany({text: 'Eat lunch'}).then((result)=> {
+  //   console.info(result);
+  // })
+  //deleteOne
+  db.collection('Todos').deleteOne({text: 'Eat Lunch'}).then((result)=> {
+    console.info(result);
+  })
+
+  //findOneAndDelete
+  // db.collection('Todos').findOneAndDelete({completed: false}).then((result)=> {
+  //   console.info(result);
+  // })
+
+
+  // db.collection('Users').deleteMany({name: 'Adi Goldstein'}).then((res)=> {
+  //   console.info(res);
+  // })
+  db.collection('Users').findOneAndDelete({ "_id" : ObjectID("599009e4d31a3f2da459e092")}).then((res)=> {
+    console.info(res);
+  })
+
+  // db.close();
+});
