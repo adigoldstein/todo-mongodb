87fa2b501f6a607e69b3c1b32b47a43662ea803d
Adi <adij82@gmail.com>
2017-08-16 20:44:02 +0300

    Minor

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -68,7 +68,7 @@ describe('GET /todos/:id', ()=> {
 
     request(app)
       .get(`/todos/${todos[0]._id.toHexString()}`)
-      .expect(404)
+      .expect(200)
       .expect((res)=> {
        expect(res.body.todo.text).toBe(todos[0].text)
       })
